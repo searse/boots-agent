af46ee292e196b2a32c6f6c17d2ec36ad080251e
searse <searse@gmail.com>
2026-02-10 12:45:18 -0800

    user input and verbose output

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
+import argparse
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -9,14 +11,31 @@ def main():
     if not api_key:
         raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User Prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=messages,
     )
+    if not response.usage_metadata:
+        raise RuntimeError("Usage Metadata Request Failed")
 
-    print("Response:")
-    print(response.text)
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print("Response:")
+        print(response.text)
+
+    else:
+        print("Response:")
+        print(response.text)
 
 
 if __name__ == "__main__":
