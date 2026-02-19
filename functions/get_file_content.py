2804a4381acdbe36c26f217de001999b1c388b86
searse <searse@gmail.com>
2026-02-19 04:57:52 -0800

    read and write file functions

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,17 +1,22 @@
 import os
 
+from config import MAX_CHARS
+
+
 def get_file_content(working_directory, file_path):
     try:
         abs_working_dir = os.path.abspath(working_directory)
-        target_file = os.path.normpath(os.path.join(abs_working_dir, file_path))
-        valid_target_file = os.path.commonpath([abs_working_dir, target_file]) == abs_working_dir
-
-        target_is_file = os.path.isfile(target_file)
-
-        if not valid_target_file:
-            return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory'
-        elif not target_is_file:
+        abs_file_path = os.path.normpath(os.path.join(abs_working_dir, file_path))
+        if os.path.commonpath([abs_working_dir, abs_file_path]) != abs_working_dir:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(abs_file_path):
             return f'Error: File not found or is not a regular file: "{file_path}"'
-
+        with open(abs_file_path, "r") as f:
+            content = f.read(MAX_CHARS)
+            if f.read(1):
+                content += (
+                    f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                )
+        return content
     except Exception as e:
-        return f"Error reading file: {e}"
+        return f'Error reading file "{file_path}": {e}'
