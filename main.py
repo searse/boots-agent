8df21f8de212f68f8c2a82cd60c7abd8da3c046b
searse <searse@gmail.com>
2026-02-21 02:07:46 -0800

    system prompt

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
 
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
@@ -27,6 +28,7 @@ def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt, temperature=0),
     )
     if not response.usage_metadata:
         raise RuntimeError("Gemeni API Metadata Request Failed")
